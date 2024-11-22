410877de8240d51be892347d2935d8545f500792
RuvimP05 <ruvim.1109@gmail.com>
2024-11-22 13:08:09 -0600

    fix labels and content height

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,11 @@ export default function Home() {
         fill="white"
         textAnchor="middle"
         dominantBaseline="central"
-        className="text-lg font-semibold"
+        className="text-lg font-semibold animate-fade-in"
+        style={{
+          animationDelay: "2s",
+          animationFillMode: "forwards",
+        }}
       >
         {value}
       </text>
@@ -264,155 +268,157 @@ export default function Home() {
         }}
       />
       <div className="flex justify-center items-center min-h-screen">
-        <div className="p-5 w-96">
-          <h1 className="text-3xl font-extrabold mb-10 text-slate-200">
-            Death Counter
-          </h1>
-          <div className="text-start">
-            {counters
-              .sort((a, b) => a.value - b.value)
-              .map((counter) => (
-                <div key={counter.id} className="my-10 flex">
-                  {isEdit ? (
+        <div className="flex w-fit">
+          <div className="flex-1 p-5 w-96">
+            <h1 className="text-3xl font-extrabold mb-10 text-slate-200">
+              Death Counter
+            </h1>
+            <div className="text-start">
+              {counters
+                .sort((a, b) => a.value - b.value)
+                .map((counter) => (
+                  <div key={counter.id} className="my-10 flex">
+                    {isEdit ? (
+                      <button
+                        className="w-14 h-10 my-auto text-slate-200 rounded-full bg-slate-800 hover:bg-slate-700 active:bg-slate-900 mr-2 transition"
+                        onClick={() => removePerson(counter.id)}
+                      >
+                        &#x2716;
+                      </button>
+                    ) : null}
+                    <div className="font-extrabold my-auto flex text-slate-200">
+                      {counter.label}:
+                    </div>
+                    <div className="flex justify-end w-full">
+                      {isEdit ? (
+                        <button
+                          onClick={() => decrementCounter(counter.id)}
+                          className="w-10 h-10 my-auto text-lg mr-2 text-slate-200 cursor-pointer bg-red-600 rounded-md active:bg-red-700 hover:bg-red-500 transition"
+                        >
+                          -
+                        </button>
+                      ) : null}
+                      <div className="w-12 h-12 text-white bg-slate-800 opacity-80 rounded-md justify-center items-center flex mr-2 z-40">
+                        {counter.value}
+                      </div>
+                      {isEdit ? (
+                        <button
+                          onClick={() => incrementCounter(counter.id)}
+                          className="w-10 h-10 my-auto text-slate-200 text-lg cursor-pointer bg-green-600 rounded-md active:bg-green-700 hover:bg-green-500 transition"
+                        >
+                          +
+                        </button>
+                      ) : null}
+                    </div>
+                  </div>
+                ))}
+              {isEdit ? (
+                <>
+                  <div className="flex justify-center items-center">
+                    <input
+                      type="text"
+                      value={newPersonName}
+                      onChange={(e) => setNewPersonName(e.target.value)}
+                      placeholder="Enter person name"
+                      className="text-slate-200 bg-slate-800 p-2 rounded-full mr-2 outline-none pl-4"
+                    />
+                    <button
+                      onClick={addPerson}
+                      className="h-10 w-full cursor-pointer bg-emerald-600 text-slate-200 rounded-full active:bg-emerald-700 hover:bg-emerald-500 transition"
+                    >
+                      Add Person
+                    </button>
+                  </div>
+                  <div className="flex">
+                    <button
+                      className="px-5 py-2 bg-blue-600 rounded-full text-slate-200 hover:bg-blue-500 active:bg-blue-700 transition mt-10 mx-auto"
+                      onClick={() => {
+                        setIsEdit(false);
+                        window.scrollTo(0, 0);
+                      }}
+                    >
+                      Done
+                    </button>
+                  </div>
+                </>
+              ) : null}
+            </div>
+          </div>
+          <div className="flex-1 p-5 w-96">
+            <h1 className="text-3xl font-extrabold mb-10 text-slate-200">
+              Causes of Death
+            </h1>
+            <div>
+              <PieChart width={400} height={400}>
+                <Pie
+                  data={causes}
+                  dataKey="value"
+                  label={renderCustomizedLabel}
+                  labelLine={false}
+                >
+                  {causes.length >= 0
+                    ? causes.map((entry, index) => (
+                        <Cell
+                          key={entry.value + 1}
+                          name={entry.cause}
+                          fill={COLORS[index % COLORS.length]}
+                        />
+                      ))
+                    : null}
+                </Pie>
+                <Legend />
+              </PieChart>
+            </div>
+            {isEdit ? (
+              <div className="text-start">
+                {causes.map((cause) => (
+                  <div key={cause.cause} className="my-10 flex">
                     <button
                       className="w-14 h-10 my-auto text-slate-200 rounded-full bg-slate-800 hover:bg-slate-700 active:bg-slate-900 mr-2 transition"
-                      onClick={() => removePerson(counter.id)}
+                      onClick={() => removeCause(cause.cause)}
                     >
                       &#x2716;
                     </button>
-                  ) : null}
-                  <div className="font-extrabold my-auto flex text-slate-200">
-                    {counter.label}:
-                  </div>
-                  <div className="flex justify-end w-full">
-                    {isEdit ? (
+                    <div className="font-extrabold my-auto flex text-slate-200">
+                      {cause.cause}:
+                    </div>
+                    <div className="flex justify-end w-full">
                       <button
-                        onClick={() => decrementCounter(counter.id)}
+                        onClick={() => decrementCause(cause.cause)}
                         className="w-10 h-10 my-auto text-lg mr-2 text-slate-200 cursor-pointer bg-red-600 rounded-md active:bg-red-700 hover:bg-red-500 transition"
                       >
                         -
                       </button>
-                    ) : null}
-                    <div className="w-12 h-12 text-white bg-slate-800 opacity-80 rounded-md justify-center items-center flex mr-2 z-40">
-                      {counter.value}
-                    </div>
-                    {isEdit ? (
+                      <div className="w-12 h-12 text-white bg-slate-800 opacity-80 rounded-md justify-center items-center flex mr-2 z-40">
+                        {cause.value}
+                      </div>
                       <button
-                        onClick={() => incrementCounter(counter.id)}
+                        onClick={() => incrementCause(cause.cause)}
                         className="w-10 h-10 my-auto text-slate-200 text-lg cursor-pointer bg-green-600 rounded-md active:bg-green-700 hover:bg-green-500 transition"
                       >
                         +
                       </button>
-                    ) : null}
+                    </div>
                   </div>
-                </div>
-              ))}
-            {isEdit ? (
-              <>
+                ))}
                 <div className="flex justify-center items-center">
                   <input
                     type="text"
-                    value={newPersonName}
-                    onChange={(e) => setNewPersonName(e.target.value)}
-                    placeholder="Enter person name"
+                    value={newCauseName}
+                    onChange={(e) => setNewCauseName(e.target.value)}
+                    placeholder="Enter cause name"
                     className="text-slate-200 bg-slate-800 p-2 rounded-full mr-2 outline-none pl-4"
                   />
                   <button
-                    onClick={addPerson}
+                    onClick={addCause}
                     className="h-10 w-full cursor-pointer bg-emerald-600 text-slate-200 rounded-full active:bg-emerald-700 hover:bg-emerald-500 transition"
                   >
-                    Add Person
+                    Add Cause
                   </button>
                 </div>
-                <div className="flex">
-                  <button
-                    className="px-5 py-2 bg-blue-600 rounded-full text-slate-200 hover:bg-blue-500 active:bg-blue-700 transition mt-10 mx-auto"
-                    onClick={() => {
-                      setIsEdit(false);
-                      window.scrollTo(0, 0);
-                    }}
-                  >
-                    Done
-                  </button>
-                </div>
-              </>
+              </div>
             ) : null}
           </div>
-        </div>
-        <div className="p-5 w-96">
-          <h1 className="text-3xl font-extrabold mb-10 text-slate-200">
-            Causes of Death
-          </h1>
-          <div>
-            <PieChart width={400} height={400}>
-              <Pie
-                data={causes}
-                dataKey="value"
-                label={renderCustomizedLabel}
-                labelLine={false}
-              >
-                {causes.length >= 0
-                  ? causes.map((entry, index) => (
-                      <Cell
-                        key={entry.value + 1}
-                        name={entry.cause}
-                        fill={COLORS[index % COLORS.length]}
-                      />
-                    ))
-                  : null}
-              </Pie>
-              <Legend />
-            </PieChart>
-          </div>
-          {isEdit ? (
-            <div className="text-start">
-              {causes.map((cause) => (
-                <div key={cause.cause} className="my-10 flex">
-                  <button
-                    className="w-14 h-10 my-auto text-slate-200 rounded-full bg-slate-800 hover:bg-slate-700 active:bg-slate-900 mr-2 transition"
-                    onClick={() => removeCause(cause.cause)}
-                  >
-                    &#x2716;
-                  </button>
-                  <div className="font-extrabold my-auto flex text-slate-200">
-                    {cause.cause}:
-                  </div>
-                  <div className="flex justify-end w-full">
-                    <button
-                      onClick={() => decrementCause(cause.cause)}
-                      className="w-10 h-10 my-auto text-lg mr-2 text-slate-200 cursor-pointer bg-red-600 rounded-md active:bg-red-700 hover:bg-red-500 transition"
-                    >
-                      -
-                    </button>
-                    <div className="w-12 h-12 text-white bg-slate-800 opacity-80 rounded-md justify-center items-center flex mr-2 z-40">
-                      {cause.value}
-                    </div>
-                    <button
-                      onClick={() => incrementCause(cause.cause)}
-                      className="w-10 h-10 my-auto text-slate-200 text-lg cursor-pointer bg-green-600 rounded-md active:bg-green-700 hover:bg-green-500 transition"
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-              ))}
-              <div className="flex justify-center items-center">
-                <input
-                  type="text"
-                  value={newCauseName}
-                  onChange={(e) => setNewCauseName(e.target.value)}
-                  placeholder="Enter cause name"
-                  className="text-slate-200 bg-slate-800 p-2 rounded-full mr-2 outline-none pl-4"
-                />
-                <button
-                  onClick={addCause}
-                  className="h-10 w-full cursor-pointer bg-emerald-600 text-slate-200 rounded-full active:bg-emerald-700 hover:bg-emerald-500 transition"
-                >
-                  Add Cause
-                </button>
-              </div>
-            </div>
-          ) : null}
         </div>
       </div>
       {errorMessage ? (
